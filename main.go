7e9d04ddbd2c8caff1fd6fbccd30734e3c1ad05d
lucas <lucas@local.host>
2021-07-22 15:13:25 -0400

    setup server

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
+	"net/http"
 )
 
 type person struct {
@@ -11,23 +9,33 @@ type person struct {
 }
 
 func main() {
-	p1 := person{First: "Wilson"}
-	p2 := person{First: "John"}
-	p3 := person{First: "Jane"}
+	// p1 := person{First: "Wilson"}
+	// p2 := person{First: "John"}
+	// p3 := person{First: "Jane"}
 
-	xp := []person{p1, p2, p3}
+	// xp := []person{p1, p2, p3}
 
-	bs, err := json.Marshal(xp)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println(string(bs))
+	// bs, err := json.Marshal(xp)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// fmt.Println(string(bs))
 
-	xp2 := []person{}
-	err = json.Unmarshal(bs, &xp2)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println(xp2)
+	// xp2 := []person{}
+	// err = json.Unmarshal(bs, &xp2)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// fmt.Println(xp2)
+
+	// http.HandleFunc("/encode", foo)
+	// http.ListenAndServe(":8080", nil)
+
+}
+func foo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
 
 }
