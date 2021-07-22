db036b33e84e7309fd40f3a313313d87e3958ecd
lucas <lucas@local.host>
2021-07-22 15:00:21 -0400

    add marshal

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First string
+}
+
+func main() {
+	p1 := person{First: "Wilson"}
+	p2 := person{First: "John"}
+	p3 := person{First: "Jane"}
+
+	xp := []person{p1, p2, p3}
+
+	bs, err := json.Marshal(xp)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(string(bs))
+
+}
