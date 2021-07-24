9d5b043cb07d92d3fc3e04b735c2e48cd49e3bfe
lucas <lucas@local.host>
2021-07-24 00:59:39 -0400

    JWT - Create claims

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/local/GoAuthentication
 
 go 1.16
 
-require golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
+)
--- main.go
+++ main.go
@@ -5,12 +5,28 @@ import (
 	"crypto/sha512"
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type person struct {
 	First string
+}
+
+type UserClaims struct {
+	jwt.StandardClaims
+	SessionId int64
+}
+
+func (u *UserClaims) Valid() error {
+	if !u.VerifyExpiresAt(time.Now().Unix(), true) {
+		return fmt.Errorf("Token has expired")
+	}
+	if u.SessionId == 0 {
+		return fmt.Errorf("Invalid Session ID")
+	}
 }
 
 var key = []byte{}
