7af7cde906d394fa38acb344a0df0b09c3a13a7a
lucas <lucas@local.host>
2021-07-26 23:19:40 -0400

    SHA256 a File

--- main.go
+++ main.go
@@ -1,161 +1,40 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/rand"
-	"crypto/sha512"
+	"crypto/sha256"
 	"fmt"
 	"io"
 	"log"
-	"time"
-
-	"github.com/dgrijalva/jwt-go"
-	"github.com/gofrs/uuid"
-	"golang.org/x/crypto/bcrypt"
+	"os"
 )
 
-type person struct {
-	First string
-}
-
-type UserClaims struct {
-	jwt.StandardClaims
-	SessionId int64
-}
-
-func (u *UserClaims) Valid() error {
-	if !u.VerifyExpiresAt(time.Now().Unix(), true) {
-		return fmt.Errorf("Token has expired")
-	}
-	if u.SessionId == 0 {
-		return fmt.Errorf("Invalid Session ID")
-	}
-
-	return nil
-}
-
 func main() {
-	pass := "123456789"
 
-	hashedPass, err := hashPassword(pass)
+	f, err := os.Open("sample-file.txt")
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
+	defer f.Close()
 
-	err = comparePassword(pass, hashedPass)
+	h := sha256.New()
+
+	_, err = io.Copy(h, f)
 	if err != nil {
-		log.Fatalln("Not logged in")
-	}
-	log.Println("Logged in!")
-
-}
-
-func hashPassword(password string) ([]byte, error) {
-	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return nil, fmt.Errorf("Error while generating bcrpyt hash from password: %w", err)
-	}
-	return bs, nil
-
-}
-
-func comparePassword(password string, hashedPass []byte) error {
-	err := bcrypt.CompareHashAndPassword(hashedPass, []byte(password))
-	if err != nil {
-		return fmt.Errorf("Invalid password: %w", err)
+		log.Fatalln("couldnt io.copy", err)
 	}
 
-	return nil
-}
+	fmt.Printf("Here's the type BEFORE Sum: %T\n", h)
+	fmt.Printf("%v\n", h)
+	xb := h.Sum(nil)
+	fmt.Printf("Here's the type AFTER Sum: %T\n", xb)
+	fmt.Printf("%x\n", xb)
 
-func signMessage(msg []byte) ([]byte, error) {
-	h := hmac.New(sha512.New, keys[currentKid].key)
+	xb = h.Sum(nil)
+	fmt.Printf("Here's the type AFTER SECOND Sum: %T\n", xb)
+	fmt.Printf("%x\n", xb)
 
-	_, err := h.Write(msg)
-	if err != nil {
-		return nil, fmt.Errorf("Error in signMessage while hashing message: %w", err)
-	}
-
-	signature := h.Sum(nil)
-	return signature, nil
-
-}
-
-func checkSig(msg, sig []byte) (bool, error) {
-	newSig, err := signMessage(msg)
-	if err != nil {
-		return false, fmt.Errorf("Error in checkSign while signature of message %w", err)
-	}
-
-	same := hmac.Equal(newSig, sig)
-	return same, nil
-
-}
-
-func createToken(c *UserClaims) (string, error) {
-	t := jwt.NewWithClaims(jwt.SigningMethodHS512, c)
-	signedToken, err := t.SignedString(keys[currentKid].key)
-	if err != nil {
-		return "", fmt.Errorf("Error in createToken when signing token: %w", err)
-	}
-
-	return signedToken, nil
-}
-
-func generateNewKey() error {
-	newKey := make([]byte, 64)
-	_, err := io.ReadFull(rand.Reader, newKey)
-	if err != nil {
-		return fmt.Errorf("Error in generateNewKey while generating new key: %w", err)
-	}
-
-	uid, err := uuid.NewV4()
-	if err != nil {
-		return fmt.Errorf("Error in generateNewKey while generating new key: %w", err)
-	}
-	keys[uid.String()] = key{
-		key:     newKey,
-		created: time.Now(),
-	}
-	currentKid = uid.String()
-	return nil
-}
-
-type key struct {
-	key     []byte
-	created time.Time
-}
-
-var keys = map[string]key{}
-var currentKid = ""
-
-func parseToken(signedToken string) (*UserClaims, error) {
-	claims := &UserClaims{}
-	t, err := jwt.ParseWithClaims(signedToken, claims, func(t *jwt.Token) (interface{}, error) {
-		if t.Method.Alg() == jwt.SigningMethodHS512.Alg() {
-			return nil, fmt.Errorf("Invalid signing algorithm")
-		}
-
-		kid, ok := t.Header["kid"].(string) // Getting the key from request Header
-		if !ok {
-			return nil, fmt.Errorf("Invalid Key ID")
-		}
-
-		k, ok := keys[kid]
-		if !ok {
-			return nil, fmt.Errorf("Invalid Key ID")
-		}
-
-		return k.key, nil
-	})
-	if err != nil {
-		return nil, fmt.Errorf("Error in parseToken while parsing token: %w", err)
-	}
-
-	if !t.Valid {
-		return nil, fmt.Errorf("Error in parseToken, token is not valid")
-	}
-
-	return t.Claims.(*UserClaims), nil
+	xb = h.Sum(xb)
+	fmt.Printf("Here's the type AFTER THIRD Sum: %T\n", xb)
+	fmt.Printf("%x\n", xb)
 
 }
