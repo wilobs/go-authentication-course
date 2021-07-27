199eced30840c212771eb49c0e4d4ad552bc906d
lucas <lucas@local.host>
2021-07-27 12:33:23 -0400

    Setting up http server

--- main.go
+++ main.go
@@ -1,40 +1,55 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
 	"io"
-	"log"
-	"os"
+	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/submit", bar)
+	http.ListenAndServe(":8081", nil)
+}
 
-	f, err := os.Open("sample-file.txt")
-	if err != nil {
-		log.Fatalln(err)
+func foo(w http.ResponseWriter, r *http.Request) {
+	html := `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>HMAC Example</title>
+	</head>
+	<body>
+		<form action="/submit" method="post">
+			<input type="email" name="email" />
+			<input type="submit" />
+		</form>
+	</body>
+	</html>
+	`
+	io.WriteString(w, html)
+}
+
+func getCode(msg string) string {
+
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 	}
-	defer f.Close()
 
-	h := sha256.New()
-
-	_, err = io.Copy(h, f)
-	if err != nil {
-		log.Fatalln("couldnt io.copy", err)
+	email := r.FormValue("email")
+	if email == "" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 	}
 
-	fmt.Printf("Here's the type BEFORE Sum: %T\n", h)
-	fmt.Printf("%v\n", h)
-	xb := h.Sum(nil)
-	fmt.Printf("Here's the type AFTER Sum: %T\n", xb)
-	fmt.Printf("%x\n", xb)
-
-	xb = h.Sum(nil)
-	fmt.Printf("Here's the type AFTER SECOND Sum: %T\n", xb)
-	fmt.Printf("%x\n", xb)
-
-	xb = h.Sum(xb)
-	fmt.Printf("Here's the type AFTER THIRD Sum: %T\n", xb)
-	fmt.Printf("%x\n", xb)
-
+	c := http.Cookie{
+		Name:  "",
+		Value: "",
+	}
 }
