9b092abc83b6123fae62a87e44468254555004f4
WilsonBolanosSoto <wilson.bolanossoto@gmail.com>
2021-07-29 00:44:28 -0400

    Code refactor

--- main.go
+++ main.go
@@ -2,45 +2,20 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"time"
-
-	"github.com/golang-jwt/jwt"
+	"net/url"
 )
 
 func main() {
-	http.HandleFunc("/", foo)
-	http.HandleFunc("/submit", bar)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/register", register)
 	http.ListenAndServe(":8081", nil)
 }
 
-func foo(w http.ResponseWriter, r *http.Request) {
-	c, err := r.Cookie("session")
-	if err != nil {
-		c = &http.Cookie{}
+func index(w http.ResponseWriter, r *http.Request) {
+	errMsg := r.FormValue("errormsg")
+	if errMsg != "" {
 	}
-
-	ss := c.Value
-	afterVerificationToken, err := jwt.ParseWithClaims(ss, &myClaims{}, func(beforeVerificationToken *jwt.Token) (interface{}, error) {
-		if beforeVerificationToken.Method.Alg() != jwt.SigningMethodHS256.Alg() {
-			return nil, fmt.Errorf("Someone tried to hack changed signing method")
-		}
-
-		return []byte(myKey), nil
-
-	})
-
-	isEqual := err == nil && afterVerificationToken.Valid
-
-	message := "Not logged in"
-	if isEqual {
-		message = "Logged in"
-		claims := afterVerificationToken.Claims.(*myClaims)
-		fmt.Println(claims.Email)
-		fmt.Println(claims.ExpiresAt)
-	}
-
 	html := `
 	<!DOCTYPE html>
 	<html lang="en">
@@ -48,73 +23,25 @@ func foo(w http.ResponseWriter, r *http.Request) {
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>HMAC Example</title>
+		<title>Document</title>
 	</head>
-	<body> 
-		<p> Cookie value: ` + c.Value + ` </p>
-		<p> Message: ` + message + ` </p>
-		<form action="/submit" method="post">
-			<input type="email" name="emailThing" />
-			<input type="submit" />
+	<body>
+		<h1>%s</h1>
+		<form action="/register" method="post">
+			<input type="email" name="e">
+			<input type="password" name="p">
+			<input type="submit">
 		</form>
 	</body>
-	</html>
-	`
-	io.WriteString(w, html)
+	</html>`
+
+	fmt.Fprintf(w, html, errMsg)
 }
 
-type myClaims struct {
-	jwt.StandardClaims
-	Email string
-}
-
-const myKey = "i love thursdays when it rains 8723 inches"
-
-func getJWT(msg string) (string, error) {
-
-	claims := myClaims{
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(time.Minute * 5).Unix(),
-		},
-		Email: msg,
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &claims)
-	ss, err := token.SignedString([]byte(myKey))
-	if err != nil {
-		return "", fmt.Errorf("Coudnt SignedString in %w", err)
-	}
-
-	return ss, nil
-
-}
-
-func bar(w http.ResponseWriter, r *http.Request) {
+func register(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
+		errorMsg := url.QueryEscape("your method was not POST")
+		http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
 		return
 	}
-
-	email := r.FormValue("emailThing")
-	if email == "" {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
-	}
-
-	ss, err := getJWT(email)
-	if err != nil {
-		http.Error(w, "couldnt getJWT", http.StatusInternalServerError)
-		return
-
-	}
-
-	c := http.Cookie{
-		Name:  "session",
-		Value: ss,
-	}
-
-	http.SetCookie(w, &c)
-	http.Redirect(w, r, "/", http.StatusSeeOther)
-
-	// "hash / message digest / digest / hash value" | "what we stored"
 }
